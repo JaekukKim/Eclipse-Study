a2c1bb248141e1208bd2c5a450a82c6b98591bd0
JaeGuk <sasameyuki03@naver.com>
2022-09-30 14:48:54 +0900

    9/30 commit

--- ThisIsJava/src/examAlone/for_method/ProblemISolved/ThreeByThreeGugudan.java
+++ ThisIsJava/src/examAlone/for_method/ProblemISolved/ThreeByThreeGugudan.java
@@ -10,9 +10,10 @@ public class ThreeByThreeGugudan {
 		// 줄관리하는건] 아무리 sysout을 도배를 해놔도 해결할수 없는 부분이였음
 		// if문으로 i나 j 3일때 줄바꾼다를 실행하는건 딱봐도 의미가 없는데 매달려있었음.
 
-		for (int i = 1; i <= 9; i += 3) {
-			for (int j = 1; j <= 9; j++) {
-				for (int k = i; k < i + 3; k++) {
+		for (int i = 1; i <= 9; i += 3) { // i의 값을 3씩 증가시켜주어서 행을 관리함.
+			for (int j = 1; j <= 9; j++) { // 구구단의 1~9까지 곱하는 수 설정
+				for (int k = i; k < i + 3; k++) { 
+	// k의 값을 i에 연관지어 행과 열을 동시에 관리 (ex)i가 1일때 k는 1부터 3까지이다. 즉 3단까지만 줄력한다는 것.
 					System.out.print(k + " x " + j + " = " + k * j + "\t");
 				}
 				System.out.println();
